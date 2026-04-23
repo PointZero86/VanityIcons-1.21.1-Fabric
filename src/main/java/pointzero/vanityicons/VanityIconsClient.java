42d782afb488b873d8c30502c342476cd7171ba3
Jorge Pabon <jorgepabon845@gmail.com>
2026-04-23 00:16:29 -0400

    Initial commit

---
+++ src/main/java/pointzero/vanityicons/VanityIconsClient.java
@@ -0,0 +1,10 @@
+package pointzero.vanityicons;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VanityIconsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/pointzero/vanityicons/VanityIconsDataGenerator.java
@@ -0,0 +1,11 @@
+package pointzero.vanityicons;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VanityIconsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
